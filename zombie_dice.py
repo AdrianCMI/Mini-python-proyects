1110245b1430c1207389d6c90430ba9d9404ed37
AdrianCM <74360332+AdrianCMI@users.noreply.github.com>
2022-02-17 14:17:00 +0100

    Update zombie_dice.py

--- zombie_dice.py
+++ zombie_dice.py
@@ -35,7 +35,7 @@ def coger_dados():
 
 
 # Cara de los dados
-def cara_random(x,n,p,z): # las variables p y z no funcionan
+def cara_random(x): # las variables p y z no funcionan
     if x <= 6:
         x = "verde"
     elif 6 < x <= 10:
@@ -47,39 +47,42 @@ def cara_random(x,n,p,z): # las variables p y z no funcionan
         x = randint(1,6)
         if x <= 3:
             x = "cerebro"
-            n -= 1
-            p += 1
         elif 3 < x <= 5:
             x = "pie"
         else:
             x = "zombie"
-            n -= 1
-            z += 1
     elif x == "amarillo":
         x = randint(1,6)
         if x <= 2:
             x = "cerebro"
-            n -= 1
-            p += 1
         elif 2 < x <= 4:
             x = "pie"
         else:
             x = "zombie"
-            n -= 1
-            z += 1
     elif x == "rojo":
         x = randint(1,16)
         if x == 1:
             x = "cerebro"
-            n -= 1
-            p += 1
         elif 1 < x <= 3:
             x = "pie"
         else:
             x = "zombie"
-            n -= 1
-            z += 1
     print(x)
+
+
+# Recuento de dados, puntos, zombies
+def calcular(dado,a,b,c):
+    global tirar_pie
+    
+    if dado == "cerebro":
+        a -= 1
+        b += 1
+    elif dado == "zombie":
+        a -= 1
+        c += 1
+    elif dado == "pie":
+        tirar_pie = "y"
+        
 
 
 # Juego
@@ -93,21 +96,23 @@ while dados_total >= 0 and jz1 or jz2 <= 3:
     color_random(dado3)
     cara = input("Siguiente...")
     print("\nY su cara es: ")
-    cara_random(dado1,dados_total,jp1,jz1)
-    cara_random(dado2,dados_total,jp1,jz1)
-    cara_random(dado3,dados_total,jp1,jz1)
+    cara_random(dado1)
+    cara_random(dado2)
+    cara_random(dado3)
+    calcular(dado1,dados_total,jp1,jz1)
+    calcular(dado2,dados_total,jp1,jz1)
+    calcular(dado3,dados_total,jp1,jz1)
     print(f"Ahora quedan {dados_total} dados")
     print(f"Tienes {jp1} puntos")
     if jz1 != 0:
         print(f"Aunque tienes {jz1} zombies")
     elif jz1 == 0:
         print("No tienes ningun zombie :)")
-    
-    if dado1 or dado2 or dado3 == "pie":
+    while dado1 == "pie" or dado2 =="pie" or dado3 == "pie" and tirar_pie == "y":
         
-        tirar_otra = input("\n\n¿Quieres tirar otra vez?(y/n): ")
+        tirar_pie = input("\n\n¿Quieres tirar otra vez?(y/n): ")
         
-        if tirar_otra == "y":
+        if tirar_pie == "y":
             
             coger_dados()
             print("\nEl color de tus dados son: ")
@@ -116,15 +121,20 @@ while dados_total >= 0 and jz1 or jz2 <= 3:
             color_random(dado3)
             cara = input("Siguiente...")
             print("\nY su cara es: ")
-            cara_random(dado1,dados_total,jp1,jz1)
-            cara_random(dado2,dados_total,jp1,jz1)
-            cara_random(dado3,dados_total,jp1,jz1)
+            cara_random(dado1)
+            cara_random(dado2)
+            cara_random(dado3)
+            calcular(dado1,dados_total,jp1,jz1)
+            calcular(dado2,dados_total,jp1,jz1)
+            calcular(dado3,dados_total,jp1,jz1)
             print(f"Ahora quedan {dados_total} dados")
             print(f"Tienes {jp1} puntos")
             if jz1 != 0:
                 print(f"Aunque tienes {jz1} zombies")
             elif jz1 == 0:
                 print("No tienes ningun zombie :)")
+        elif tirar_pie == "n":
+            break
             
     inicioj2 = input("\n\nAhora le toca al jugador 2:\nTira los dados...")
     coger_dados()
@@ -134,21 +144,23 @@ while dados_total >= 0 and jz1 or jz2 <= 3:
     color_random(dado3)
     cara = input("Siguiente...")
     print("\nY su cara es: ")
-    cara_random(dado1,dados_total,jp2,jz2)
-    cara_random(dado2,dados_total,jp2,jz2)
-    cara_random(dado3,dados_total,jp2,jz2)
+    cara_random(dado1)
+    cara_random(dado2)
+    cara_random(dado3)
+    calcular(dado1,dados_total,jp2,jz2)
+    calcular(dado2,dados_total,jp2,jz2)
+    calcular(dado3,dados_total,jp2,jz2)
     print(f"Ahora quedan {dados_total} dados")
     print(f"Tienes {jp2} puntos")
     if jz2 != 0:
         print(f"Aunque tienes {jz2} zombies")
     elif jz2 == 0:
         print("No tienes ningun zombie :)")
+    while dado1 == "pie" or dado2 == "pie" or dado3 == "pie" and tirar_pie == "y":
         
-    if dado1 or dado2 or dado3 == "pie":
-        
-        tirar_otra = input("\n\n¿Quieres tirar otra vez?(y/n): ")
+        tirar_pie = input("\n\n¿Quieres tirar otra vez?(y/n): ")
 
-        if tirar_otra == "y":
+        if tirar_pie == "y":
             coger_dados()
             print("\nEl color de tus dados son: ")
             color_random(dado1)
@@ -156,14 +168,19 @@ while dados_total >= 0 and jz1 or jz2 <= 3:
             color_random(dado3)
             cara = input("Siguiente...")
             print("\nY su cara es: ")
-            cara_random(dado1,dados_total,jp1,jz2)
-            cara_random(dado2,dados_total,jp1,jz2)
-            cara_random(dado3,dados_total,jp1,jz2)
+            cara_random(dado1)
+            cara_random(dado2)
+            cara_random(dado3)
+            calcular(dado1,dados_total,jp2,jz2)
+            calcular(dado2,dados_total,jp2,jz2)
+            calcular(dado3,dados_total,jp2,jz2)
             print(f"Ahora quedan {dados_total} dados")
             print(f"Tienes {jp2} puntos")
             if jz2 != 0:
                 print(f"Aunque tienes {jz2} zombies")
             elif jz2 == 0:
                 print("No tienes ningun zombie :)")
+        elif tirar_pie == "n":
+            break
 
 print("\nHa acabado el juego")
